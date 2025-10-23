37473e6125bf68087f287a2bfb24678c7c3eec6a
dnwldla <leeemjw@naver.com>
2025-10-23 10:49:45 +0900

    feat: Car 클래스 추가

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private final String name;
+    private int count;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void move(){
+        int randomNum= Randoms.pickNumberInRange(1,9);
+
+        if (randomNum>=4){
+            this.count++;
+        }
+    }
+
+}
