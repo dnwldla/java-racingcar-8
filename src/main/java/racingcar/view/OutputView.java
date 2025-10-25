9854165db02e8114951c66e5baabdd7ea0335ecc
dnwldla <leeemjw@naver.com>
2025-10-25 18:10:20 +0900

    feat: 출력을 담당하는 클래스 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,18 @@
+package racingcar.view;
+
+public class OutputView {
+
+    public void printResultPrompt(){
+        System.out.println("실행 결과");
+    }
+
+    public void printProgress(String progress){
+        System.out.println(progress);
+    }
+
+    public void printWinner(String winners){
+        System.out.printf("최종 우승자 : %s",winners);
+
+    }
+
+}
